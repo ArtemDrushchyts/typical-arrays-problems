cff6185713053d38d139958bc4db5f9612ae1c8b
Artem Drushchyts <temafm-3@yandex.ru>
2020-02-23 15:39:56 +0300

    feat: complete task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  let min = array[0];
+
+  if(array.length == 0){
+    min = 0;
+  } else {
+    for(let i = 1; i < array.length; i++) {
+      if(array[i] < min) {
+        min = array[i];
+      }
+    }
+  }
+  
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  let max = array[0];
+  if(array.length == 0) {
+    max = 0;
+  } else {
+    for(let i = 1; i < array.length; i++) {
+      if(array[i] > max) {
+        max = array[i];
+      }
+    }
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  return array.length == 0 ? 0 : array.reduce((prev, curent) => prev + curent) / array.length;
 }
